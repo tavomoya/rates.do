9358f34bdbf09a1790db50cf7034f90b3f2cd679
Gustavo Moya <gmoyatorres@gmail.com>
2016-05-16 00:31:04 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var request = require('request');
+var q = require('q');
+
+// Mixin Constructor
+function Rates () {
+
+  // Define API Path
+  this.apiPath = 'http://api.marcos.do';
+
+};
+
+// Get rates from all banks
+Rates.prototype.getAllRates = function () {
+  var deferred = q.defer();
+  var _this = this;
+
+  request.get(_this.apiPath+'/rates',
+  function (err, res){
+    if (err) {
+      deferred.reject(err);
+    };
+
+    if (!res) {
+      deferred.reject({
+        msg: 'An unexpected error occured while fetching the rates'
+      });
+    } else {
+      deferred.resolve({
+        rates: res.body,
+        status: res.statusCode
+      });
+    };
+  });
+
+  return deferred.promise;
+};
+
+module.exports = Rates;
